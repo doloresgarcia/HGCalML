0d38e3803bcb6c4381dfe83b370880396f70361c
Jan Kieseler <jkiesele@cern.ch>
2023-09-22 06:55:55 -0400

    env for wandb

--- env.sh
+++ env.sh
@@ -1,13 +1,3 @@
-export HGCALML=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
-export DEEPJETCORE_SUBPACKAGE=$HGCALML
-export PATH=$HGCALML/scripts:$PATH
-export PYTHONPATH=$HGCALML/modules:$PYTHONPATH
-export LD_LIBRARY_PATH=$HGCALML/modules:$LD_LIBRARY_PATH
-export LC_ALL=C.UTF-8 	# necessary for wandb
-export LANG=C.UTF-8	# necessary for wandb
-#?export PYTHONPATH=$HGCALML/modules/datastructures:$PYTHONPATH
-#for ffmpeg
-
 
 export PATH=$PATH:/afs/cern.ch/work/g/gkrzmanc/pypack
 export PYTHONPATH=$PYTHONPATH:/afs/cern.ch/work/g/gkrzmanc/pypack/
@@ -18,4 +8,9 @@ export LANG=C.UTF-8
 export PATH=$PATH:/mnt/proj3/dd-23-91/cern/pypack
 export PYTHONPATH=$PYTHONPATH:/mnt/proj3/dd-23-91/cern/pypack
 
+# wandb api key
+# if ~/private/wandb_api.sh file exists then source from it
+if [[ -f ~/private/wandb_api.sh ]]; then
+   source ~/private/wandb_api.sh
+fi
 
