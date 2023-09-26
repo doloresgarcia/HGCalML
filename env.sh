e43dab4c7f67e221c751c5dbbd1ff0484c9be5c0
Gregor Kržmanc <gregor.krzmanc@gmail.com>
2023-09-26 18:52:50 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -5,6 +5,13 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
 
+export HGCALML=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
+export DEEPJETCORE_SUBPACKAGE=$HGCALML
+
+export PATH=$HGCALML/scripts:$PATH
+export PYTHONPATH=$HGCALML/modules:$PYTHONPATH
+export LD_LIBRARY_PATH=$HGCALML/modules:$LD_LIBRARY_PATH
+
 export PATH=$PATH:/mnt/proj3/dd-23-91/cern/pypack
 export PYTHONPATH=$PYTHONPATH:/mnt/proj3/dd-23-91/cern/pypack
 
